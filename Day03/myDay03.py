21957050de61312d62591b880664711e19224207
Sumayyea Salahuddin <sumayya.malik@gmail.com>
2023-10-10 19:25:01 +0500

    third commit

---
+++ Day03/myDay03.py
@@ -0,0 +1,18 @@
+# Day 03
+# Celsius to Fahrenheit converter using Python function
+
+# Create function tempConvert()
+def tempConvert():
+    # Input temperature in Celsius
+    celsius = float(input("Enter temperature in Celsius: "))
+
+    # Convert Celsius to Fahrenheit
+    fahrenheit = (celsius * 9/5) + 32
+    
+    # Display the result
+    print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit.")
+
+tempConvert()		# Function Call in Python
+tempConvert()		# Another function call in Python
+
+
